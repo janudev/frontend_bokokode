52df16e3a64189eb14b697839e29d22605eb2ff0
janudev <alejandromorenocarrasco@gmail.com>
2024-11-04 13:34:55 +0100

    Paginación

--- src/app/products/features/product-list/product-list.component.ts
+++ src/app/products/features/product-list/product-list.component.ts
@@ -3,8 +3,8 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../../interfaces/Product';
 import { CommonModule } from '@angular/common';
-import { FeaturedProductCardComponent } from "../../components/featured-product-card/featured-product-card.component"
-import { ProductCardComponent } from "../../components/product-card/product-card.component"
+import { FeaturedProductCardComponent } from "../../components/featured-product-card/featured-product-card.component";
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
 
 @Component({
   selector: 'app-product-list',
@@ -21,35 +21,56 @@ import { ProductCardComponent } from "../../components/product-card/product-card
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
   featuredProd: Product | undefined;
+
+  // Filtrado
   isAsc: boolean = true;
   selectedCategories: string[] = [];
   categories: string[] = ['People', 'Premium', 'Pets', 'Food', 'Landmarks', 'Cities', 'Nature'];
   sortKey: string = 'price';
   sortType: string = 'ASC';
+  
+  // Paginación
+  currentPage: number = 1;
+  pageSize: number = 6;
+  totalProducts: number = 0;
+  totalPages: number = 0;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
     this.getProducts(this.sortKey, this.selectedCategories);
-  }  
+  }
 
   getProducts(sortKey: string = this.sortKey, categories: string[] = this.selectedCategories): void {
-    this.productService.getProducts(sortKey, this.sortType, categories).subscribe(
-      (products) => {
-        if (!this.featuredProd) {
-          this.featuredProd = products.find(product => product.featured === true);
-        }
+    const page = this.currentPage;
+    this.productService.getProducts(sortKey, this.sortType, categories, page, this.pageSize).subscribe(
+      ({ products, total }) => {
         
+        const featuredProducts = products.filter(product => product.featured === true);
+
+        this.featuredProd = featuredProducts.length > 0 ? featuredProducts[0] : undefined;
         this.products = products.filter(product => product.featured !== true);
-  
-        if (categories.length > 0) {
-          this.products = this.products.filter(product => categories.includes(product.category));
-        }
+        this.totalPages = Math.ceil(total / this.pageSize);
+        
       },
       (err) => console.error("Error al obtener los productos:", err)
     );
-  }  
-  
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.getProducts(this.sortKey, this.selectedCategories);
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.getProducts(this.sortKey, this.selectedCategories);
+    }
+  }
+
   toggleSortByPrice(): void {
     this.isAsc = !this.isAsc;
     this.sortType = this.isAsc ? 'ASC' : 'DESC';
--- src/app/products/services/product.service.ts
+++ src/app/products/services/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Product } from "../../interfaces/Product";
-import { Observable, BehaviorSubject } from 'rxjs'
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ export class ProductService {
   private cart: Product[] = [];
   private cartCount = new BehaviorSubject<number>(0);
 
-  BASE_URL: string = "http://localhost:3000"
+  BASE_URL: string = "http://localhost:3000";
 
   constructor(private http: HttpClient) { }
 
@@ -18,7 +18,7 @@ export class ProductService {
     return this.http.get<Product>(`${this.BASE_URL}/products/${id}`);
   }
 
-  getProducts(sortKey?: string, sortType?: string, categories?: string[]): Observable<Product[]> {
+  getProducts(sortKey?: string, sortType?: string, categories?: string[], page: number = 1, pageSize: number = 6): Observable<{ products: Product[], total: number }> {
     const body: Record<string, any> = {};
   
     if (sortKey) {
@@ -27,9 +27,12 @@ export class ProductService {
     if (categories && categories.length > 0) {
       body['categories'] = categories;
     }
-  
-    return this.http.post<Product[]>(`${this.BASE_URL}/products`, body);
-  }
+    
+    body['page'] = page;
+    body['pageSize'] = pageSize;
+
+    return this.http.post<{ products: Product[], total: number }>(`${this.BASE_URL}/products`, body);
+  } 
 
   cartCount$ = this.cartCount.asObservable();
 
@@ -46,5 +49,4 @@ export class ProductService {
     this.cart = [];
     this.cartCount.next(this.cart.length);
   }
-  
 }
